7a691f62a9eeba9451d763dcd91ba923fa019195
Eric Lundell <emlundell@alaska.edu>
2020-10-01 17:48:05 -0800

    Move general_cpu tests into script. Some software returns "normal" non-zero error codes. This messes with Docker's way of doing things.

---
+++ general_cpu/tests.sh
@@ -0,0 +1,35 @@
+set -e
+
+python --version 
+python3.8 --version 
+python2 --version
+python2.7 --version
+
+# ISCE
+python3.8 $ISCE_HOME/applications/topsApp.py --help && [ $? -gt 2 ] && true
+python3.8 -c "import isce; print(isce.__version__)"
+python3.8 -c "from isce.applications import topsApp"
+
+# gdal
+gdalwarp --version
+
+# mapready
+asf_mapready -version
+asf_geocode -version
+
+# MintPy
+python3.8 -c "import numpy"
+python3.8 -c "from mintpy import view, tsview, plot_network, plot_transection, plot_coherence_matrix"
+smallbaselineApp.py --help
+
+# ARIA
+ariaDownload.py --help
+
+# Train
+python3.8 /usr/local/TRAIN/src/aps_weather_model.py -h
+
+# giant
+python2.7 /usr/local/GIAnT/SCR/ProcessStack.py -h
+
+# snap
+/usr/local/snap/bin/gpt -h
