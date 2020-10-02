cfe656d457b3e00288543a2782a6edbe51664069
Eric Lundell <emlundell@alaska.edu>
2020-10-02 15:41:28 -0800

    Put Others first

--- codebuild_build.sh
+++ codebuild_build.sh
@@ -1,9 +1,10 @@
 
 
-export DOCKER_REGISTRY=localhost:5000
+export DOCKER_REGISTRY=localhost:5000  #CONTAINER_REPO=553778890976.dkr.ecr.us-east-1.amazonaws.com/asf-franz-labs
 export AWS_PROFILE=jupyterhub
 export STAGE_MATURITY=test
 export STAGE_LOCATION=remote
+export FORCED_LIST=()
 
 STAGES_PATH_LIST=(aria base finale giant isce mapready mintpy snap train )
 PROFILES_PATH_LIST=( general_cpu )
@@ -19,22 +20,6 @@ echo "Current directory contents:"
 ls $TOP_DIR
 echo ""
 
-for THIS_STAGE in ${STAGES_PATH_LIST[@]};
-do
-    cd $TOP_DIR
-    echo "Checking $THIS_STAGE in stages..."
-    if [[ " ${CHANGES[@]} " =~ stages/$THIS_STAGE-stage ]]
-    then 
-        if [ -d "stages/$THIS_STAGE-stage" ]
-        then
-            cd stages
-            bash build.sh $THIS_STAGE
-        else
-            echo "No path named stages/$THIS_STAGE"
-        fi
-    fi
-done
-
 for THIS_PATH in ${OTHERS_PATH_LIST[@]};
 do
     cd $TOP_DIR
@@ -47,6 +32,22 @@ do
             bash build.sh
         else
             echo "No path named others/$THIS_PATH"
+        fi
+    fi
+done
+
+for THIS_STAGE in ${STAGES_PATH_LIST[@]};
+do
+    cd $TOP_DIR
+    echo "Checking $THIS_STAGE in stages..."
+    if [[ " ${CHANGES[@]} " =~ stages/$THIS_STAGE-stage ]]
+    then 
+        if [ -d "stages/$THIS_STAGE-stage" ]
+        then
+            cd stages
+            bash build.sh $THIS_STAGE
+        else
+            echo "No path named stages/$THIS_STAGE"
         fi
     fi
 done
