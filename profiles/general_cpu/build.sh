b5a2e5c66ad15a418a516a8d794f8055b46341e2
Eric Lundell <emlundell@alaska.edu>
2020-10-06 22:40:13 -0800

    PROFILE_MATURITY

--- profiles/general_cpu/build.sh
+++ profiles/general_cpu/build.sh
@@ -2,15 +2,15 @@
 set -ex
 
 cp dockerfile dockerfile.build
-sed -i "s|base-stage:|$DOCKER_REGISTRY/base-stage:$STAGE_MATURITY|g" dockerfile.build
-SED_STR="s|--from=(.*):|--from=$DOCKER_REGISTRY/\1:$STAGE_MATURITY|g"
+sed -i "s|base-stage:|$DOCKER_REGISTRY/base-stage:$PROFILE_MATURITY|g" dockerfile.build
+SED_STR="s|--from=(.*):|--from=$DOCKER_REGISTRY/\1:$PROFILE_MATURITY|g"
 sed -i -r $SED_STR dockerfile.build
 
 BUILD_TAG=$(date +"%F-%H-%M-%S")
 
 time docker build -f dockerfile.build --target testing .
-docker build -f dockerfile.build -t $DOCKER_REGISTRY/general_cpu:$BUILD_TAG -t $DOCKER_REGISTRY/general_cpu:$STAGE_MATURITY --target release .
+docker build -f dockerfile.build -t $DOCKER_REGISTRY/general_cpu:$BUILD_TAG -t $DOCKER_REGISTRY/general_cpu:$PROFILE_MATURITY --target release .
 
 # Push to registry
 docker push $DOCKER_REGISTRY/general_cpu:$BUILD_TAG
-docker push $DOCKER_REGISTRY/general_cpu:$STAGE_MATURITY
+docker push $DOCKER_REGISTRY/general_cpu:$PROFILE_MATURITY
