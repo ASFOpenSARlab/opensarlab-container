124f13e28780ef3dad57aec78136d5d6d56adc7c
Eric Lundell <emlundell@alaska.edu>
2020-10-07 15:56:21 -0800

    Build on merge diffs. Or on normal diffs if STAGE_COMPARE_MERGES=false

--- stages/build.sh
+++ stages/build.sh
@@ -1,25 +1,36 @@
-set -e
+set -ex
 
 STAGE_NAME=$1
-BUILD_TAG=$(date +"%F-%H-%M-%S")
 
-echo "Building '$DOCKER_REGISTRY/$STAGE_NAME' with tags '$BUILD_TAG' and '$STAGE_MATURITY' for location '$STAGE_LOCATION' "
+if [ "$STAGE_COMPARE_MERGES" != 'false' ]; then
+    GIT_MERGE_HASHES=( $(git log --merges --first-parent HEAD --pretty=format:"%h") )
+else
+    GIT_MERGE_HASHES=( $(git rev-parse --short HEAD) )  
+fi
 
-cd $STAGE_NAME-stage
-[ -e download.sh ] && bash download.sh
+BUILD_TAG=${GIT_MERGE_HASHES[0]}
+MERGE_CHANGES_ARRAY=$(git diff --name-only ${GIT_MERGE_HASHES[1]} ${GIT_MERGE_HASHES[0]})
+echo "Changes in files: ${MERGE_CHANGES_ARRAY[@]}"
 
-cp dockerfile dockerfile.build
-sed -i "s|base-stage:|$DOCKER_REGISTRY/base-stage:$STAGE_MATURITY|g" dockerfile.build
+if [[ " ${MERGE_CHANGES_ARRAY[@]} " =~ $STAGE_NAME-stage ]]; then
 
-SED_STR="s|--from=(.*):|--from=$DOCKER_REGISTRY/\1:$STAGE_MATURITY|g"
-sed -i -r $SED_STR dockerfile.build
+    echo "Building '$DOCKER_REGISTRY/$STAGE_NAME' with tags '$BUILD_TAG' and '$STAGE_MATURITY' for location '$STAGE_LOCATION' "
 
-time docker build -f dockerfile.build --target $STAGE_NAME-stage-test .
-time docker build -f dockerfile.build -t $DOCKER_REGISTRY/$STAGE_NAME-stage:$BUILD_TAG -t $DOCKER_REGISTRY/$STAGE_NAME-stage:$STAGE_MATURITY --target $STAGE_NAME-stage --no-cache .
+    cd $STAGE_NAME-stage
+    [ -e download.sh ] && bash download.sh
 
-# Push image to registry
-if [ "$STAGE_LOCATION" != 'local' ]
-then
-    docker push $DOCKER_REGISTRY/$STAGE_NAME-stage:$BUILD_TAG
-    docker push $DOCKER_REGISTRY/$STAGE_NAME-stage:$STAGE_MATURITY
+    cp dockerfile dockerfile.build
+    sed -i "s|base-stage:|$DOCKER_REGISTRY/base-stage:$STAGE_MATURITY|g" dockerfile.build
+
+    SED_STR="s|--from=(.*):|--from=$DOCKER_REGISTRY/\1:$STAGE_MATURITY|g"
+    sed -i -r $SED_STR dockerfile.build
+
+    time docker build -f dockerfile.build --target $STAGE_NAME-stage-test .
+    time docker build -f dockerfile.build -t $DOCKER_REGISTRY/$STAGE_NAME-stage:$BUILD_TAG -t $DOCKER_REGISTRY/$STAGE_NAME-stage:$STAGE_MATURITY --target $STAGE_NAME-stage --no-cache .
+
+    # Push image to registry
+    if [ "$STAGE_REMOTE_PUSH" != 'false' ]; then
+        docker push $DOCKER_REGISTRY/$STAGE_NAME-stage:$BUILD_TAG
+        docker push $DOCKER_REGISTRY/$STAGE_NAME-stage:$STAGE_MATURITY
+    fi
 fi
--- stages/local-build-all.sh
+++ stages/local-build-all.sh
@@ -1,11 +1,12 @@
 
 set -e
 
-#DOCKER_REGISTRY=localhost:5000 AWS_PROFILE=jupyterhub STAGE_MATURITY=test STAGE_LOCATION=local
+#DOCKER_REGISTRY=localhost:5000 AWS_PROFILE=jupyterhub STAGE_MATURITY=test STAGE_REMOTE_PUSH=false STAGE_COMPARE_MERGES=false
 export DOCKER_REGISTRY=localhost:5000
 export AWS_PROFILE=jupyterhub
 export STAGE_MATURITY=test
-export STAGE_LOCATION=remote
+export STAGE_REMOTE_PUSH=false
+export STAGE_COMPARE_MERGES=false
 
 for j in \
     base \
