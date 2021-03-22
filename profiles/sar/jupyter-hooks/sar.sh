7d2d84d4f7e56b091068a79010f7c182cd7bd59c
Eric Lundell <emlundell@alaska.edu>
2021-03-22 10:20:53 -0800

    Missing `sar` changes

---
+++ profiles/sar/jupyter-hooks/sar.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+python /etc/jupyter-hooks/resource_checks/check_storage.py $1
+
+pip install --user \
+    nbgitpuller \
+    ipywidgets \
+    mpldatacursor \
+    rise \
+    hide_code \
+    jupyter_nbextensions_configurator \
+    pandoc==2.0a4 \
+    pypandoc
+
+# copy over our version of pull.py
+# REMINDER: REMOVE IF CHANGES ARE MERGED TO NBGITPULLER
+cp /etc/jupyter-hooks/scripts/pull.py /home/jovyan/.local/lib/python3.8/site-packages/nbgitpuller/pull.py
+
+# Add Path to local pip execs. 
+export PATH=$HOME/.local/bin:$PATH
+
+jupyter serverextension enable --py nbgitpuller
+jupyter nbextensions_configurator enable --user
+jupyter nbextension enable --py widgetsnbextension --user
+jupyter-nbextension enable rise --py --user 
+jupyter nbextension install --py hide_code --user
+jupyter nbextension enable --py hide_code --user
+jupyter serverextension enable --py hide_code --user
+
+mkdir -p $HOME/.ipython/profile_default/startup/
+cp /etc/jupyter-hooks/custom_magics/00-df.py $HOME/.ipython/profile_default/startup/00-df.py
+
+gitpuller https://github.com/asfadmin/asf-jupyter-docs.git master $HOME/opensarlab_docs
+python /etc/jupyter-hooks/scripts/osl_user_guides_to_ipynb.py -p $HOME/opensarlab_docs
