a1569e5486cae3b24caa20ae915de218b053c379
Eric Lundell <emlundell@alaska.edu>
2021-04-29 16:02:41 -0800

    Tag docker push with git commit

--- profiles/sar/build.sh
+++ profiles/sar/build.sh
@@ -14,12 +14,18 @@ if [ "$STAGE_FORCE_BUILD" = 'true' ]; then
     cp -r ../../tests/* tests/
 
     BUILD_TAG=$(date +"%F-%H-%M-%S")
+    COMMIT_HEAD=$(git rev-parse --short HEAD)
 
     time docker build -f dockerfile.build --target testing .
-    time docker build -f dockerfile.build -t $DOCKER_REGISTRY/sar:$BUILD_TAG -t $DOCKER_REGISTRY/sar:$PROFILE_MATURITY --target release .
+    time docker build -f dockerfile.build \
+        -t $DOCKER_REGISTRY/sar:$BUILD_TAG \
+        -t $DOCKER_REGISTRY/sar:$PROFILE_MATURITY \
+        -t $DOCKER_REGISTRY/sar:$COMMIT_HEAD \
+        --target release .
 
     # Push to registry
     docker push $DOCKER_REGISTRY/sar:$BUILD_TAG
     docker push $DOCKER_REGISTRY/sar:$PROFILE_MATURITY
+    docker push $DOCKER_REGISTRY/sar:$COMMIT_HEAD
 
 fi
