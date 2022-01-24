4951a753a55449768a569005614378d5975fa10e
Alex-Lewandowski <aflewandowski@alaska.edu>
2022-01-24 12:50:22 -0900

    update for jupyter lab

--- tests/aria.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-# The idiom "&& [ $? -gt 2 ] && true" ignores warnings from the previous command
-
-ariaDownload.py --help
--- tests/snap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-# The idiom "&& [ $? -gt 2 ] && true" ignores warnings from the previous command
-
-/usr/local/snap/bin/gpt -h
-#snap --help && [ $? -gt 2 ] && true
