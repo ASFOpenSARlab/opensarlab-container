273d1d321b1ec7cd1592bb4f88124b7fb1d1dc7a
Eric Lundell <emlundell@alaska.edu>
2023-10-23 08:46:27 -0800

    Comment out TRAIN. Rearrange layers.

--- tests/train.sh
+++ tests/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+#set -ex
 # The idiom "&& [ $? -gt 2 ] && true" ignores warnings from the previous command
 
-python3.9 /usr/local/TRAIN/src/aps_weather_model.py -h
+#python3 /usr/local/TRAIN/src/aps_weather_model.py -h
